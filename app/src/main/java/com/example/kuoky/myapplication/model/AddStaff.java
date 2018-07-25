56de672fa8b2d8e9c10568217b11080ec01aa2ed
lchlgyxx <leefk-wa15@student.tarc.edu.my>
2018-07-25 13:57:10 +0800

    compile change to implementation

---
+++ app/src/main/java/com/example/kuoky/myapplication/model/AddStaff.java
@@ -0,0 +1,15 @@
+package com.example.kuoky.myapplication.model;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.kuoky.myapplication.R;
+
+public class AddStaff extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_staff);
+    }
+}
